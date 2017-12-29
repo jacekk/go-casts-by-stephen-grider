741842b58aac33a1d56ae538cf26a83803e1d59b
jacekk <gitkraken@jacek-k.net>
2017-12-29 01:35:09 +0100

    improved task solution - added another way for looping;

--- 03-odd-or-even/main.go
+++ 03-odd-or-even/main.go
@@ -6,12 +6,27 @@ import (
 
 func main() {
 	numbers := make([]int, 11)
-
 	for number := range numbers {
-		desc := "odd"
-		if number%2 == 0 {
-			desc = "even"
-		}
-		fmt.Printf("%2.1d is %s \n", number, desc)
+		printNumber(number)
 	}
+
+	fmt.Println()
+	fmt.Println("OR:")
+	fmt.Println()
+
+	index := 0
+	for index <= 10 { // 10 <= 10 gives true; 11 <= 10 gives false
+		printNumber(index)
+		index++
+	}
+}
+
+func printNumber(number int) {
+	desc := "odd"
+
+	if number%2 == 0 {
+		desc = "even"
+	}
+
+	fmt.Printf("%2.1d is %s \n", number, desc)
 }
